c232f5b5787655e36385772711ca13aa392d3104
prot-alx <prot.alx.92@gmail.com>
2024-06-05 21:39:36 +0500

    test

--- src/app/app-entry/index.tsx
+++ src/app/app-entry/index.tsx
@@ -3,14 +3,14 @@ const App = () => {
     <div className="font-lato">
       <div className="text-2xl font-bold shadow-lg mb-8">1234567890</div>
       <div className="grid grid-cols-4 gap-4">
-        <div className="w-11 h-11 bg-danger"></div>
+          <div className="w-11 h-11 bg-danger"></div>
         <div className="shadow-lg w-11 h-11 bg-success"></div>
         <div className="shadow-xl w-11 h-11 bg-warning"></div>
         <div className="shadow-2xl w-11 h-11 bg-info"></div>
         <div className="shadow-md w-11 h-1"></div>
         <div className="shadow-lg w-11 h-11"></div>
         <div className="shadow-xl w-11 h-11"></div>
-        
+
         <div className="shadow- w-11 h-11"></div>
 
         <div className="text-large text-gray-900">ASDFGHJJKLZXCBNM</div>
