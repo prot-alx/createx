7efae10e7b67da71f2e740dcd78cb1d8fe5d3e9c
prot-alx <prot.alx.92@gmail.com>
2024-06-06 09:30:37 +0500

    first buttons test

--- src/app/app-entry/index.tsx
+++ src/app/app-entry/index.tsx
@@ -1,23 +1,25 @@
+import Button from "@/shared/buttons/basic/large";
+
 const App = () => {
   return (
     <div className="font-lato">
-      <div className="text-2xl font-bold shadow-lg mb-8">1234567890</div>
-      <div className="grid grid-cols-4 gap-4">
-        <div className="w-11 h-11 bg-danger"></div>
-        <div className="shadow-lg w-11 h-11 bg-success"></div>
-        <div className="shadow-xl w-11 h-11 bg-warning"></div>
-        <div className="shadow-2xl w-11 h-11 bg-info"></div>
-
-        <div className="shadow-md w-11 h-1"></div>
-        <div className="shadow-lg w-11 h-11"></div>
-        <div className="shadow-xl w-11 h-11"></div>
-        <div className="shadow-regular w-11 h-11"></div>
-
-        <div className="text-large text-gray-900">ASDFGHJJKLZXCBNM</div>
-        <div className="text-small text-gray-900">asdfgfgjghkwefwegdfbdfb</div>
-        <div className="text-extraextrasmall text-gray-900">1233456467867897890</div>
-        <div className="text-lead text-gray-900">knijoasnA</div>
+      <div className="p-6 space-x-4">
+        <Button buttonStyle={{ color: 'green', rounded: 'lg', size: 'md' }}>
+          Button
+        </Button>
+        <Button buttonStyle={{ color: 'teal', rounded: 'lg', size: 'md' }}>
+          Button
+        </Button>
+        <Button buttonStyle={{ color: 'yellow', rounded: 'lg', size: 'md' }}>
+          Button
+        </Button>
+        <Button buttonStyle={{ color: 'gray', rounded: 'lg', size: 'md' }}>
+          Button
+        </Button>
       </div>
+      <Button buttonStyle={{ color: 'green', rounded: 'lg', size: 'md' }}>
+        Button
+      </Button>
     </div>
   )
 }
